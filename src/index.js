ab01b1836676820ab2a345e3d944ce0de6897bde
kokis128 <jorgevalentini76@gmail.com>
2022-09-04 08:50:22 -0300

    --

--- src/index.js
+++ src/index.js
@@ -2,21 +2,10 @@ require('dotenv').config;
 var app = require('./app')
 require('./database');
 var port = 4000;
-<<<<<<< HEAD
 async function main() {       
 
         await app.listen(port)
         console.log('Server on port ' , app.get('port'))
-=======
-
-
-
-async function main() {
-               
-
-        await app.listen(port)
-        console.log('Server on port 4000')  
->>>>>>> dev
 }
 
 main();

03cfad692dd049582be078269fe4f89aea4d7a8b
kokis128 <jorgevalentini76@gmail.com>
2022-09-04 10:03:39 -0300

    finsh

--- src/app.js
+++ src/app.js
@@ -8,17 +8,13 @@ app.set('port', process.env.PORT || 4000);
 
 app.use(cors());
 
-const cors=require('cors');
 
-<<<<<<< HEAD
-//middlewares
-=======
-app.use(cors());
+
+
 app.use(express.json());
 
 app.use('/api/users',require('./routes/users'));
 app.use('/api/notes',require('./routes/notes'));
->>>>>>> dev
 
 module.exports = app;
 app.use(express.json());
@@ -26,5 +22,3 @@ app.use(express.json());
 
 //routes
 
-app.get('/api/users', (req,res) => res.send('Users Routes'));
-app.get('/api/notes',(req,res)=>res.send('Notes Routes'));

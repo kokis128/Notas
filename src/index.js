e1fbfba0160a610bd80fd2526399ae58290d8ac0
kokis128 <jorgevalentini76@gmail.com>
2022-09-04 09:12:46 -0300

    --

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require('./database');
 var port = 4000;
 async function main() {       
 
-        await app.listen(port)
+         app.listen(port)
         console.log('Server on port ' , app.get('port'))
 }
 
